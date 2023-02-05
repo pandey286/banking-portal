ab52f5307e0d485df73a02bec43ff4bb906f9709
Prashant Pandey <78202155+pandey286@users.noreply.github.com>
2023-02-05 14:34:42 +0530

    Admin Dashboard added
    Co-authored-by: Sayeri21 <Sayeri21@users.noreply.github.com>

--- src/components/Dashboard/Admin/AdminDashBoard.js
+++ src/components/Dashboard/Admin/AdminDashBoard.js
@@ -1,21 +1,174 @@
-// import React, { Component } from 'react';
-// import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './admindash.css'
 
-// class AdminDashboard extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <div class="card" style="width: 18rem;">
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//   </div>
-// </div>
-//       </div>
-//     );
-//   }
-// }
+class AdminDashboard extends Component {
+  render() {
+    return (
+      <Container>
+        <Row className="mt-5">
 
-// export default AdminDashboard;
+
+          <Col xs={12}>
+            <h1>Welcome to the Admin Dashboard<button className='btn btn-danger'><Link to="/">Logout</Link></button></h1>
+
+          </Col>
+        </Row>
+        <Row>
+          <Form inline>
+
+
+
+            <div className="viewAll">
+
+              <form className="form-card">
+                <p className="p"><h3>User Credentials</h3></p>
+                <div class="form-grid">
+
+                  <div class="form-row">
+                    <label>Name</label>
+                    <input type="text" />
+                  </div>
+                  <div class="form-row">
+
+                    <label>Account Number</label>
+                    <input type="text" />
+                  </div>
+
+                  <div class="form-row">
+                    <label> Account Type</label>
+                    <input type="text" />
+                  </div>
+
+                  <div class="form-row">
+                    <label> PAN_No</label>
+                    <input type="text" />
+                  </div>
+                </div>
+
+                <div className="text-center pt-4">
+                  <button type="submit" className="btn btn-primary">Search User</button>
+                </div>
+              </form>
+
+              <div className="col data-table mt-4">
+                <h5 className="mb-2">User Database</h5>
+               
+                <table className="table bg-white shadow-sm  text-center table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">User_Id</th>
+                      <th scope="col">User_Name</th>
+                      <th scope="col">User_Email</th>
+                      <th scope="col">Adhar_Card</th>
+                      <th scope="col">PAN_Card</th>
+                      <th scope="col">Transactions</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Contact_Info</th>
+                      <th scope="col">DOB</th>
+                      <th scope="col">Address</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle success"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle success"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle success"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle failed"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle failed"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle failed"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                    <tr>
+                      <td>TRANS0000012</td>
+                      <td>@zoro</td>
+                      <td>zoro12@example.com</td>
+                      <td>7726-8766-2983</td>
+                      <td>KJQP0890L</td>
+                      <td>TRANS0000012</td>
+                      <td className="fa fa-check-circle success"></td>
+                      <td>1234567897</td>
+                      <td>08.3.1999</td>
+                      <td>Bangalore,India</td>
+                    </tr>
+                  </tbody>
+                </table>
+                
+              </div>
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-danger">Delete User</button>
+            </div>
+          </Form>
+        </Row>
+
+      </Container>
+    );
+  }
+}
+
+export default AdminDashboard;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/footer";
-import Login from "../components/Logins/Login";
 import Navbar from "../components/Navbar";
 
 const Layout = () => {
