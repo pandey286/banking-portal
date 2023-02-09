3915235c01793d4d72548db5ffd872424603736d
Prashant Pandey <78202155+pandey286@users.noreply.github.com>
2023-02-10 00:56:39 +0530

    Login Validation Happening in frontend

--- src/components/Logins/Login.js
+++ src/components/Logins/Login.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 import Nav from '../Navbar';
 import LogImg from './images/LogImg1.gif';
 import axios from 'axios';
-
+import { FaUserCircle } from 'react-icons/fa';
+import swal from 'sweetalert';
 
 function Login() {
+
+    const [errorMessage, setErrorMessage] = useState('');
 
     const [formData, setFormData] = useState({
         email: '',
@@ -21,15 +24,48 @@ function Login() {
 
     const url = "http://localhost:8080/api/customers/login"
 
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-        try {
-          const response = await axios.post(url, formData);
-          console.log(response.data);
-        } catch (error) {
-          console.error(error);
+        setErrorMessage('');
+
+        if (!formData.email) {
+            setErrorMessage('Email is required');
         }
-      };
+        else if (!formData.password) {
+            setErrorMessage('Password is required');
+        }
+        else if (!formData.email || !formData.password) {
+            setErrorMessage('Email and password are required');
+        }
+        else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            setErrorMessage('Invalid email address');
+            if (formData.password.length < 6){
+                setErrorMessage('Invalid Credentials');
+            }
+        } else if (formData.password.length < 6) {
+            setErrorMessage('Password must be at least 6 characters long');
+        }
+        else {
+            try {
+                const response = await axios.post(url, formData);
+                swal({
+                    title: "Login Succesful",
+                    text: "Your Login Was Successful",
+                    icon: "success",
+                });
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+                swal({
+                    title: "Login UnSuccessFul !",
+                    text: "Please Check Your Credential",
+                    icon: "warning",
+                });
+                setErrorMessage('Incorrect email or password');
+            }
+        }
+    };
 
 
     return (
@@ -41,7 +77,8 @@ function Login() {
                 </div>
                 <div className='login-container d-flex justify-content-center m-5 p-5 col '>
                     <div className='row shadow-lg p-3 mb-5 bg-body-tertiary rounded-3 p-3'>
-                        <h2 className='text-center p-3 '>Please Login Here</h2>
+                        <h2 className='text-center p-3 '>Please Login Here</h2><br />
+                        <span className='text-center fs-1'><FaUserCircle /></span>
                         <form>
                             <div className="p-3 mb-3 col align-self-center">
                                 <input type="email" className="form-control shadow-sm  " id="userId" aria-describedby="EmailId" name='email' onChange={handleChange}
@@ -49,10 +86,11 @@ function Login() {
                             </div>
                             <div className="p-3 col align-self-center">
                                 <input type="password" className="form-control  shadow-sm " id="InputPassword1" name='password' onChange={handleChange}
-                                     placeholder="Enter Registered Password" />
+                                    placeholder="Enter Registered Password" />
                             </div>
+                            {errorMessage && <div className='alert alert-danger mt-3' role='alert'>{errorMessage}</div>}
                             <div className="text-center p-3">
-                                <button type="submit" onClick={handleSubmit} className="btn btn-outline-primary shadow p-1 mb-5 rounded-1">Login</button><br />
+                                <button type="submit" onClick={handleSubmit} className="btn btn-outline-primary shadow p-2 mb-5 rounded-1">Login</button><br />
                                 <Link className="text-danger fs-5" to="/forgetpass">Forgot password?</Link>
                             </div>
                             <div className="text-center p-3">
