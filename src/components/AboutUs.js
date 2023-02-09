cf21a940b4b8ccdf84b9c0e4ce2111fce15451cf
Prashant Pandey <78202155+pandey286@users.noreply.github.com>
2023-02-10 01:26:17 +0530

    Prashant Social Media Link and Photo added

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Prash from '../images/AboutImg/Prashant.jpg'
 
 const AboutUs = () => {
   return (
@@ -38,13 +39,13 @@ const AboutUs = () => {
 
             {/* <!-- Team item --> */}
             <div className="col-xl-3 col-sm-6 mb-5">
-              <div className="bg-white rounded shadow mb-5 py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-2.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+              <div className="bg-white rounded shadow mb-5 py-5 px-4"><img src={Prash} alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
                 <h5 className="mb-0">Prashant Pandey</h5><span className="small text-uppercase text-muted">CEO - Founder</span>
                 <ul className="social mb-0 list-inline mt-3">
-                  <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-facebook-f"></i></a></li>
-                  <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-twitter"></i></a></li>
-                  <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-instagram"></i></a></li>
-                  <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-linkedin"></i></a></li>
+                  <li className="list-inline-item"><a href="https://www.facebook.com/profile.php?id=100012775172400" target="_blank" className="social-link" ><i className="fa fa-facebook-f"></i></a></li>
+                  <li className="list-inline-item"><a href="https://twitter.com/Prashan72867242" target="_blank" className="social-link"><i className="fa fa-twitter"></i></a></li>
+                  <li className="list-inline-item"><a href="https://www.instagram.com/pandey__prashant__/"  target="_blank" className="social-link"><i className="fa fa-instagram"></i></a></li>
+                  <li className="list-inline-item"><a href="https://www.linkedin.com/in/prashant-pandey-57191a216/" target="_blank" className="social-link"><i className="fa fa-linkedin"></i></a></li>
                 </ul>
               </div>
             </div>
